c820b810f6931050d611adb3878bba22c5758d2d
gacp90 <ivsmca90@gmail.com>
2026-05-28 16:27:54 -0400

    cloud

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -124,7 +124,7 @@ export class ChatService {
       messaging_product: 'whatsapp',
       recipient_type: 'individual',
       to: to,
-      type: metaType,
+      type: finalMimeType,
     };
 
     if (metaType === 'image') metaPayload.image = { link: publicUrl };
